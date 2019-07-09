66ffd5d197b4961e5d057687a91f85441fc7eff3
joab sigrah <jksiggy@gmail.com>
2019-07-09 13:26:07 -0500

    practice

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            wordsAndDefinitions.Add("Cool", "When I get a job");
+            wordsAndDefinitions.Add("Fun", "A lot a food to eat");
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            Console.WriteLine(wordsAndDefinitions["Awesome"]);
+
+            
+      foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+      {
+        Console.WriteLine($"The definition of {word.Value} word {word.Key}");
+      }
+           
+            
+
+            
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
